757336fe05c216a5b0cef52da2134f71d88849bb
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-09 15:29:21 +0700

    chore: Initial nix usage

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "null2264's ThinkPad L460 OpenCore Hackintosh";
+
+  outputs = { self, nixpkgs, utils, oceanix, ... }:
+    utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+      {
+        packages = rec {
+          thinkpad-l460 = (oceanix.lib.OpenCoreConfig {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ oceanix.overlays.default ];
+            };
+
+            modules = [
+              ./nix/modules
+            ];
+          }).efiPackage;
+          default = thinkpad-l460;
+        };
+      });
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+
+    utils.url = "github:numtide/flake-utils";
+
+    oceanix = {
+      url = "github:LEXUGE/oceanix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
+  };
+}
---
+++ nix/modules/default.nix
@@ -0,0 +1,27 @@
+{ lib, pkgs, ... }:
+
+{
+  config.oceanix.opencore.resources = {
+    # ACPIFolders = [ ../../Patches ];
+    # KextsFolders = [ ../../Kexts ];
+    # DriversFolders = [ ../../Drivers ];
+    packages = [
+      # pkgs.airportitlwm-latest-ventura
+      pkgs.itlwm-v2_1_0
+      pkgs.applealc-latest-release
+      pkgs.brightnesskeys-latest-release
+      pkgs.ecenabler-latest-release
+      pkgs.intel-bluetooth-firmware-latest
+      pkgs.brcmpatchram-latest-release
+      pkgs.nvmefix-latest-release
+      pkgs.virtualsmc-latest-release
+      pkgs.whatevergreen-latest-release
+      pkgs.lilu-latest-release
+      pkgs.voodooi2c-latest
+      pkgs.voodoops2controller-latest-release
+      pkgs.rtcmemoryfixup-latest-release
+      pkgs.intel-mausi-latest-release
+      pkgs.usbtoolbox-latest-release
+    ];
+  };
+}
