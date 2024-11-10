37e84f025b02895338d99b03b6ecc09085203c85
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-10 09:44:10 +0700

    refactor: Reformat code

--- flake.nix
+++ flake.nix
@@ -2,22 +2,23 @@
   description = "null2264's ThinkPad L460 OpenCore Hackintosh";
 
   outputs = { self, nixpkgs, utils, oceanix, ... }:
-    utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
-      {
-        packages = rec {
-          thinkpad-l460 = (oceanix.lib.OpenCoreConfig {
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [ oceanix.overlays.default ];
-            };
+    utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
+    (system:
+    {
+      packages = rec {
+        l460 = (oceanix.lib.OpenCoreConfig {
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ oceanix.overlays.default ];
+          };
 
-            modules = [
-              ./nix/modules
-            ];
-          }).efiPackage;
-          default = thinkpad-l460;
-        };
-      });
+          modules = [
+            ./nix/modules
+          ];
+        }).efiPackage;
+        default = l460;
+      };
+    });
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
