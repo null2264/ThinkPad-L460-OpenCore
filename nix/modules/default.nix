2116f18cbc416ca7de9cd4be6b3e5341b7023b8a
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-10 08:19:54 +0700

    chore: Update oceanix to remove AppleALCU

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -1,27 +1,34 @@
 { lib, pkgs, ... }:
 
 {
-  config.oceanix.opencore.resources = {
-    # ACPIFolders = [ ../../Patches ];
-    # KextsFolders = [ ../../Kexts ];
-    # DriversFolders = [ ../../Drivers ];
-    packages = [
-      pkgs.oc.airportitlwm.latest-ventura
-      # pkgs.oc.itlwm.latest
-      # pkgs.oc.applealc.latest
-      # pkgs.oc.brightnesskeys.latest
-      # pkgs.oc.ecenabler.latest
-      # pkgs.oc.intel-bluetooth-firmware.latest
-      # pkgs.oc.brcmpatchram.latest
-      # pkgs.oc.nvmefix.latest
-      # pkgs.oc.virtualsmc.latest
-      # pkgs.oc.whatevergreen.latest
-      # pkgs.oc.lilu.latest
-      # pkgs.oc.voodooi2c.latest
-      # pkgs.oc.voodoops2controller.latest
-      # pkgs.oc.rtcmemoryfixup.latest
-      # pkgs.oc.intel-mausi.latest
-      # pkgs.oc.usbtoolbox.latest
-    ];
+  kexts.applealc = {
+    enable = true;
+    type = "alc";
+  };
+
+  oceanix.opencore = {
+    validate = false;
+    resources = {
+      # ACPIFolders = [ ../../Patches ];
+      # KextsFolders = [ ../../Kexts ];
+      # DriversFolders = [ ../../Drivers ];
+      packages = [
+        pkgs.oc.airportitlwm.latest-ventura
+        # pkgs.oc.itlwm.latest
+        # pkgs.oc.brightnesskeys.latest
+        # pkgs.oc.ecenabler.latest
+        # pkgs.oc.intel-bluetooth-firmware.latest
+        # pkgs.oc.brcmpatchram.latest
+        # pkgs.oc.nvmefix.latest
+        # pkgs.oc.virtualsmc.latest
+        # pkgs.oc.whatevergreen.latest
+        # pkgs.oc.lilu.latest
+        # pkgs.oc.voodooi2c.latest
+        # pkgs.oc.voodoops2controller.latest
+        # pkgs.oc.rtcmemoryfixup.latest
+        # pkgs.oc.intel-mausi.latest
+        # pkgs.oc.usbtoolbox.latest
+      ];
+    };
   };
 }
