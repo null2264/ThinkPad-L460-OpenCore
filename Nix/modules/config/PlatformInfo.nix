767116e5dd202c171d2b9bfb370042ae165bceb3
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-15 13:19:43 +0700

    oceanix: Add missing `Generic:ROM` and `Generic:SystemProductName`

--- Nix/modules/config/PlatformInfo.nix
+++ Nix/modules/config/PlatformInfo.nix
@@ -1,5 +1,7 @@
 { lib, pkgs, ... }:
 
+with lib.oc.plist;
+
 let
   findValue = var: if builtins.pathExists ../../include/${var} then (builtins.readFile ../../include/${var}) else "";
   mlb = findValue "mlb";
@@ -7,8 +9,12 @@ let
   systemUUID = findValue "uuid";
 in {
   oceanix.opencore.settings.PlatformInfo = {
-    Generic.MLB = if mlb == "" then "M0000000000000001" else mlb;
-    Generic.SystemSerialNumber = if serialNumber == "" then "W00000000001" else serialNumber;
-    Generic.SystemUUID = if systemUUID == "" then "00000000-0000-0000-0000-000000000000" else systemUUID;
+    Generic = {
+      MLB = if mlb == "" then "M0000000000000001" else mlb;
+      SystemSerialNumber = if serialNumber == "" then "W00000000001" else serialNumber;
+      SystemUUID = if systemUUID == "" then "00000000-0000-0000-0000-000000000000" else systemUUID;
+      ROM = mkData "VOGtP43i";  # NOTE: Recommended to be changed to your own MAC Address
+      SystemProductName = "MacBookPro16,3";
+    };
   };
 }
