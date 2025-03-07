a2391069ca436489fce5d5211fae9f92c344cbfb
Ahmad Ansori Palembani <palembani@gmail.com>
2025-03-08 05:49:19 +0700

    chore(nix): Change SMBIOS to use MacBookPro14,2
    I'm not planning to use Sonoma, at least not for the time being.

--- Nix/modules/config/PlatformInfo.nix
+++ Nix/modules/config/PlatformInfo.nix
@@ -3,6 +3,7 @@
 with lib.oc.plist;
 
 let
+  # NOTE: Use GenSMBIOS to get values for MLB, Serial Number and UUID
   findValue = var: if builtins.pathExists ../../include/${var} then (builtins.readFile ../../include/${var}) else "";
   mlb = findValue "mlb";
   serialNumber = findValue "serialnumber";
@@ -14,7 +15,8 @@ in {
       SystemSerialNumber = if serialNumber == "" then "W00000000001" else serialNumber;
       SystemUUID = if systemUUID == "" then "00000000-0000-0000-0000-000000000000" else systemUUID;
       ROM = mkData "VOGtP43i";  # NOTE: Recommended to be changed to your own MAC Address
-      SystemProductName = "MacBookPro16,3";
+      # Change this to MacBookPro16,3 for Sonoma support
+      SystemProductName = "MacBookPro14,2";
     };
   };
 }
