332766efa0a60361568d92904c37fe7c4e2e8b70
Ahmad Ansori Palembani <palembani@gmail.com>
2025-03-13 06:26:41 +0700

    refactor(nix): Adjust flake.nix

--- flake.nix
+++ flake.nix
@@ -2,28 +2,23 @@
   description = "null2264's ThinkPad L460 OpenCore Hackintosh";
 
   outputs = { self, nixpkgs, utils, oceanix, ... }:
-    utils.lib.eachDefaultSystem (
-      system:
-      let
-        l460 = oceanix.lib.OpenCoreConfig {
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ oceanix.overlays.default ];
-          };
-
-          modules = [
-            ./Nix/modules
-          ];
+  {
+    opencoreConfigurations = (utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] (system: {
+      l460 = oceanix.lib.OpenCoreConfig {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ oceanix.overlays.default ];
         };
-      in
-      {
-        packages = {
-          default = l460.efiPackage;
-        };
-      }
-    );
 
-  inputs = {
+        modules = [
+          ./Nix/modules
+        ];
+      };
+    }));
+  };
+
+  inputs =
+  {
     nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
 
     utils.url = "github:numtide/flake-utils";
